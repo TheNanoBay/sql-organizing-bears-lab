09add4297cdc51beca86e0c40aae36a09af890ab
TheNanoBay <noorani.mehdi@outlook.com>
2020-12-21 11:56:56 -0600

    commit tree 435b50635c0d3cd6989be94261c6d957f7cccf08

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 12, 'M', 'black', 'GRR', true); 
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 14, 'M', 'yellow', 'woohoo', true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 1, 'F', 'brown', 'yaaas', false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 44, 'M', 'black', 'urah', false);   
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 31, 'F', 'purple', 'noooo', true); 
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 102, 'M', 'green', 'mad', false); 
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'red', 'smiley', true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 12, 'M', 'black', 'GRR', true);  
